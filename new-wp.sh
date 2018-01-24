1fa565be72dbf1f89dc303f9b22f956faec9e582
Jay Holtslander <j.holtslander@gmail.com>
2018-01-24 14:34:15 -0800

    Update new-wp.sh

--- new-wp.sh
+++ new-wp.sh
@@ -3,12 +3,12 @@
 clear
 
 # PROMPT USER FOR THE SITE'S URL
-echo What is the domain name and extension of the new site? eg: skunkworks.ca
+echo What is the domain name and extension of the new site? eg: wordpress.org
 
-# MAKE THE RESPONSE A VARIABLE
+# MAKE THE USER'S RESPONSE A NEW VARIABLE
 read domainname
 
-# DOWNLOAD WORDPRESS TO A FOLDER IN MAMP'S HTDOCS FOLDER AND NAMED BY THE VARIABLE
+# DOWNLOAD THE LATEST WORDPRESS TO A NEW FOLDER IN MAMP'S HTDOCS FOLDER, NAMED BY THE VARIABLE
 echo Downloading Wordpress to the new folder $domainname in the MAMP htdocs folder.
 echo 
 echo wget -P /Applications/MAMP/htdocs/$domainname http://wordpress.org/latest.tar.gz
@@ -19,12 +19,12 @@ echo cd /Applications/MAMP/htdocs/$domainname
 cd /Applications/MAMP/htdocs/$domainname
 echo
 
-# UNZIP THE WORDPRESS ZIP FILE NOW LOCATED THERE
+# NOW UNZIP THE WORDPRESS ZIP FILE LOCATED IN THERE
 echo Unzipping...
 tar xfz latest.tar.gz
 echo
 
-# MOVE THE FILES ONE LEVEL UP
+# MOVE THE UNZIPPED FILES ONE LEVEL UP
 echo Cleaning up...
 mv wordpress/* ./
 echo
@@ -32,7 +32,7 @@ echo
 # GET RID OF THE NOW EMPTY WORDPRESS FOLDER
 rmdir ./wordpress/
 
-# GET RID OF THE WORDPRESS ZIP FILE
+# GET RID OF THE DOWNLOADED AND UNZIPPED WORDPRESS ZIP FILE
 rm -f latest.tar.gz
 
 echo All done! The $domainname site folder is now ready within MAMPs htdocs folder.
